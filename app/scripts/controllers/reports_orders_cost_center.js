9963758018648f58871d7ba79f6cb9d304c9a9c7
Vasil Iliev <vasililiev@hotmail.com>
2016-10-28 13:25:41 +0200

    Cost center report

--- app/scripts/controllers/reports_orders_cost_center.js
+++ app/scripts/controllers/reports_orders_cost_center.js
@@ -26,67 +26,83 @@ angular.module('canteenApp')
     vm.availableDates = [];
     vm.costCenters =[];
 
+    //for dates form
+    vm.dateOptions = {
+        formatYear: 'yyyy',
+        maxDate: new Date(2020, 5, 22),
+        minDate: new Date(2016, 1, 1),
+        startingDay: 1
+    };
+
+    vm.dateFrom = {
+        selected: ((new Date()).setDate((new Date()).getDate() - 20)),
+        open: false
+
+    };
+
+    vm.dateTo = {
+        selected: ((new Date()).setDate((new Date()).getDate() + 20)),
+        open: false
+    };
+
 	vm.GetOrdersPerCostCenter = function(){
 		vm.progressBar.setColor('#8dc63f');
 	    vm.progressBar.start();
     	var dateF = $filter('date')(vm.dateFrom.selected, "yyyy-MM-dd HH:mm:ss.sss");
     	var dateT = $filter('date')(vm.dateTo.selected, "yyyy-MM-dd HH:mm:ss.sss");
 
+    	var params = {
+        	dateFrom : dateF,
+        	dateTo: dateT,
+    	};
+
+    	if(vm.costCenter != null && vm.costCenter != undefined && vm.costCenter != "")
+    		params.costCenterID = vm.costCenter;
+
 		$http({
-	            method: 'GET',
-	            crossDomain: true,
-	            url: APP_CONFIG.BASE_URL + APP_CONFIG.reports_orders_cost_center,
-	            params:
-	            {
-	            	dateFrom : dateF,
-	            	dateTo: dateT,
-	            	costCenterID: vm.CostCenter
-	            }
-	        }).
-	        success(function(data) {
-	        	vm.progressBar.complete();
-	            vm.orders = data;
-	            vm.table = new ngTableParams({
-	              page: 1,
-	              count: 5
-	            }, {
-	              total: data.length,
-	              //Hide the count div
-	              //counts: [],
-	              getData: function($defer, params) {
-	                var filter = params.filter();
-	                var sorting = params.sorting();
-	                var count = params.count();
-	                var page = params.page();
-	                $defer.resolve(data.slice((page - 1) * count, page * count));
-	              }
-	          });
-	        }).
-	        error(function(data, status, headers, config) {
-	        	vm.progressBar.reset();
-	            toastr.error("Грешка при преземање податоци. Обидете се повторно!");
-	        });
+            method: 'GET',
+            crossDomain: true,
+            url: APP_CONFIG.BASE_URL + APP_CONFIG.reports_orders_cost_center,
+            params: params
+        }).
+        success(function(data) {
+        	vm.progressBar.complete();
+            vm.orders = data;
+
+            for(var i in data) {
+            	var date = data[i];
+            	var total = 0;
+            	for(var j in date.CostCenters) {
+            		var center = date.CostCenters[j];
+
+            		var centerSpan = center.ShiftOne.length + center.ShiftTwo.length + center.ShiftTwo.length;
+            		center.span = centerSpan;
+            		total += centerSpan;
+            	}
+                date.span = total;
+            }
+
+            vm.table = new ngTableParams({
+              page: 1,
+              count: 5
+            }, {
+              total: data.length,
+              //Hide the count div
+              //counts: [],
+              getData: function($defer, params) {
+                var filter = params.filter();
+                var sorting = params.sorting();
+                var count = params.count();
+                var page = params.page();
+                $defer.resolve(data.slice((page - 1) * count, page * count));
+              }
+          });
+        }).
+        error(function(data, status, headers, config) {
+        	vm.progressBar.reset();
+            toastr.error("Грешка при преземање податоци. Обидете се повторно!");
+        });
 	};
-
-	//for dates form
-
-	vm.dateOptions = {
-	    formatYear: 'yyyy',
-	    maxDate: new Date(2020, 5, 22),
-	    minDate: new Date(2016, 1, 1),
-	    startingDay: 1
-  	};
-
-    vm.dateFrom = {
-    	selected: null,
-    	open: false
-
-    };
-
-    vm.dateTo = {
-    	selected: null,
-    	open: false
-    };
 
     vm.openDateFrom = function() {
     	vm.dateFrom.open = !vm.dateFrom.open;
@@ -122,17 +138,17 @@ angular.module('canteenApp')
 
     vm.getCostCenters = function(){
     	$http({
-          method: 'GET',
-          crossDomain: true,
-          url:  APP_CONFIG.BASE_URL + APP_CONFIG.costcenter
-      }).
-      success(function(data) {/*
-        console.log("Success getting cost centers");*/
-        vm.costCenters = data;
-      }).
-      error(function(data, status, headers, config) {
-          console.log("Error getting cost centers");
-      });
+	          method: 'GET',
+	          crossDomain: true,
+	          url:  APP_CONFIG.BASE_URL + APP_CONFIG.costcenter
+	    }).
+	    success(function(data) {/*
+	        console.log("Success getting cost centers");*/
+	        vm.costCenters = data;
+	    }).
+	    error(function(data, status, headers, config) {
+	        console.log("Error getting cost centers");
+	    });
     };
 	
 	vm.getCostCenters();
